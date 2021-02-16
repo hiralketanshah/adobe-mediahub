839fbb872f68a8020dd2b32b872127e624ecce66
Grégory Stiévenard <gregory.stievenard@gmail.com>
2021-02-16 18:47:07 +0100

    Fix date format

--- ui.apps/src/main/content/jcr_root/apps/mediahub/projects/wizards/clientlibs/js/limitMaxDate.js
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/projects/wizards/clientlibs/js/limitMaxDate.js
@@ -2,7 +2,7 @@
     "use strict";
     $(document).on("change", "coral-datepicker#externalUserDatepicker", function (e) {
         var calendar = e.target;
-        var value = moment(calendar.value);
+        var value = moment(calendar.value, calendar.valueFormat);
         if (value.isAfter(moment().add(1, 'y'))) {
             alert("Expiration date must be less than a year");
             calendar.valueAsDate = moment().add(364, 'd').toDate();
