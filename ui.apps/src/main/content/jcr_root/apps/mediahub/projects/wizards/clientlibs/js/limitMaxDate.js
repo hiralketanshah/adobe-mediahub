fd6c255b872544027c4744a8b22ac07084c82d20
Grégory Stiévenard <gregory.stievenard@gmail.com>
2021-02-18 13:41:24 +0100

    Fix typo

--- ui.apps/src/main/content/jcr_root/apps/mediahub/projects/wizards/clientlibs/js/limitMaxDate.js
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/projects/wizards/clientlibs/js/limitMaxDate.js
@@ -4,7 +4,7 @@
         var calendar = e.target;
         var value = moment(calendar.value, calendar.valueFormat);
         if (value.isAfter(moment().add(1, 'y'))) {
-            ui.alert(Granite.I18n.get("Error"), Granite.I18n.get("Expiration date must be less than a year"), 'error');
+            ui.alert(Granite.I18n.get("Error"), Granite.I18n.get("Expiration date must be less than a year from today"), 'error');
             calendar.valueAsDate = moment().add(364, 'd').toDate();
         }
     });
