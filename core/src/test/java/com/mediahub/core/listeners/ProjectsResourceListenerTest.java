3f873a05deee8cfb767e443cec3d584ba7337d63
Hiral Shah <hishah@adobe.com>
2021-07-27 17:19:55 +0530

    [feature/sprint_3_junit_coverage] Updated ProjectsResourceListenerTest

--- core/src/test/java/com/mediahub/core/listeners/ProjectsResourceListenerTest.java
+++ core/src/test/java/com/mediahub/core/listeners/ProjectsResourceListenerTest.java
@@ -13,9 +13,12 @@ import javax.jcr.Node;
 import javax.jcr.Property;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.Value;
+import javax.jcr.ValueFactory;
 import javax.jcr.Workspace;
 import javax.jcr.observation.EventIterator;
 import javax.jcr.observation.ObservationManager;
+import javax.jcr.security.AccessControlEntry;
 import javax.jcr.security.AccessControlManager;
 import javax.jcr.security.AccessControlPolicy;
 import javax.jcr.security.Privilege;
@@ -23,6 +26,10 @@ import javax.jcr.security.Privilege;
 import org.apache.jackrabbit.api.JackrabbitSession;
 import org.apache.jackrabbit.api.security.JackrabbitAccessControlList;
 import org.apache.jackrabbit.api.security.principal.PrincipalManager;
+import org.apache.jackrabbit.api.security.user.Authorizable;
+import org.apache.jackrabbit.api.security.user.Group;
+import org.apache.jackrabbit.api.security.user.User;
+import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.jackrabbit.commons.jackrabbit.authorization.AccessControlUtils;
 import org.apache.sling.api.SlingConstants;
 import org.apache.sling.api.resource.LoginException;
@@ -30,6 +37,7 @@ import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.resource.observation.ResourceChange;
 import org.apache.sling.api.resource.observation.ResourceChange.ChangeType;
 import org.junit.jupiter.api.AfterEach;
@@ -38,11 +46,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.event.Event;
 
 import com.mediahub.core.constants.BnpConstants;
+import com.mediahub.core.utils.CreatePolicyNodeUtil;
 
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
@@ -110,15 +120,37 @@ public class ProjectsResourceListenerTest {
     PrincipalManager principalManager;
 
     @Mock
+    UserManager userManager;
+
+    @Mock
     private AccessControlPolicy accessControlPolicy;
 
     @Mock
     private Privilege privilege;
 
     @Mock
+    private Group group;
+
+    @Mock
+    ValueFactory vf;
+
+    @Mock
+    Value value;
+
+    @Mock
+    ValueMap valueMap;
+
+    @Mock
     private ModifiableValueMap modifiableValueMap;
 
+    @Mock
+    private AccessControlEntry accessControlEntry;
+
+    @Mock
+    User user;
+
     private MockUp<AccessControlUtils> accessControlUtilsMockup;
+    private MockUp<CreatePolicyNodeUtil> createPolicyNodeUtilMockup;
 
     Map<String, Object> authInfo = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE,
             BnpConstants.WRITE_SERVICE);
@@ -133,21 +165,62 @@ public class ProjectsResourceListenerTest {
 
     @Test
     public void onEventTest() throws Exception, RepositoryException {
+
+        List<Authorizable> userList = new ArrayList<>();
+        userList.add(user);
+
+        AccessControlEntry entry = new AccessControlEntry() {
+
+            @Override
+            public Privilege[] getPrivileges() {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public Principal getPrincipal() {
+                // TODO Auto-generated method stub
+                return new Principal() {
+
+                    @Override
+                    public String getName() {
+                        // TODO Auto-generated method stub
+                        return "Test Name";
+                    }
+                };
+            }
+        };
+
+        AccessControlEntry[] entries = new AccessControlEntry[1];
+        entries[0] = entry;
+
         when(resourceResolverFactory.getServiceResourceResolver(authInfo)).thenReturn(resolver);
         when(resolver.adaptTo(Session.class)).thenReturn(jackrabbitSession);
-        when(resolver.getResource("/content/projects")).thenReturn(resource);
+        when(resolver.getResource(Mockito.any(String.class))).thenReturn(resource);
         when(resource.getResourceType()).thenReturn("cq/gui/components/projects/admin/card/projectcard");
         when(resource.adaptTo(Node.class)).thenReturn(node);
+        when(resource.getChild("jcr:content")).thenReturn(resource);
+        when(resource.adaptTo(ModifiableValueMap.class)).thenReturn(modifiableValueMap);
+        when(resource.getValueMap()).thenReturn(valueMap);
+        when(valueMap.get("project.path", String.class)).thenReturn("Test Project Path");
+        when(valueMap.get("damFolderPath", String.class)).thenReturn("Test DAM folder path");
         when(parentresource.adaptTo(Node.class)).thenReturn(node);
         when(jackrabbitSession.getPrincipalManager()).thenReturn(principalManager);
+        when(jackrabbitSession.getUserManager()).thenReturn(userManager);
         when((node.getProperty(BnpConstants.ROLE_EDITOR))).thenReturn(property);
         when(node.getProperty(BnpConstants.ROLE_OBSERVER)).thenReturn(property);
         when(node.getProperty(BnpConstants.ROLE_OWNER)).thenReturn(property);
         when(node.getProperty(BnpConstants.ROLE_PROJECTPUBLISHER)).thenReturn(property);
         when(node.getProperty(BnpConstants.ROLE_EXTERNALCONTRIBUTEUR)).thenReturn(property);
+        when(node.hasProperty(BnpConstants.ROLE_EDITOR)).thenReturn(true);
         when(property.getString()).thenReturn("projects-bnpproject-editor");
         when(principalManager.getPrincipal("projects-bnpproject-editor")).thenReturn(principal);
         when(resource.getParent()).thenReturn(parentresource);
+        when(parentresource.getPath()).thenReturn(BnpConstants.MEDIALIBRARY_PATH);
+        when(userManager.getAuthorizable(Mockito.anyString())).thenReturn(group);
+        when(group.getMembers()).thenReturn(userList.iterator());
+        when(jackrabbitSession.getValueFactory()).thenReturn(vf);
+        when(vf.createValue("")).thenReturn(value);
 
         when(node.hasNode(BnpConstants.REP_POLICY)).thenReturn(true);
         when(jackrabbitSession.getAccessControlManager()).thenReturn(accessControlManager);
@@ -158,6 +231,16 @@ public class ProjectsResourceListenerTest {
                 return jackrabbitAccessControlList;
             }
         };
+
+        createPolicyNodeUtilMockup = new MockUp<CreatePolicyNodeUtil>() {
+
+            @mockit.Mock
+            void createRepPolicyNodes(Session adminSession, String parentFolderPath, List<Principal> principalNameList,
+                    Map<String, Value> restrictions) {
+            }
+        };
+
+        when(jackrabbitAccessControlList.getAccessControlEntries()).thenReturn(entries);
 
         Privilege[] privileges = new Privilege[] { accessControlManager.privilegeFromName(Privilege.JCR_READ) };
         when(jackrabbitAccessControlList.addEntry(principal, privileges, true)).thenReturn(true);
