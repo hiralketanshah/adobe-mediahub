a93638212c5f258696feba79a1d8631bf6057dd4
Gregory Stievenard <gregory.stievenard@gmail.com>
2021-08-09 19:16:51 +0200

    Fix typo

--- ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/aem-assets-metadata-file-size-units/file-size-units.js
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/aem-assets-metadata-file-size-units/file-size-units.js
@@ -1,45 +1,45 @@
-(function($, $document) {
+(function ($, $document) {
     var FILE_SIZE_NAME = "./jcr:content/metadata/dam:size",
-    FORMATTED_ASSET_SIZE = "./jcr:content/metadata/asset:size",
-    initialized = false;
+        FORMATTED_ASSET_SIZE = "./jcr:content/metadata/asset:size",
+        initialized = false;
 
     $document.on("foundation-contentloaded", init);
- 
-    function init(){
-        if(initialized){
+
+    function init() {
+        if (initialized) {
             return;
         }
- 
+
         initialized = true;
- 
+
         convertFileSize();
     }
 
-    function convertFileSize(){
+    function convertFileSize() {
         var $damSize = document.getElementsByName(FILE_SIZE_NAME);
         var $formattedAssetSize = document.getElementsByName(FORMATTED_ASSET_SIZE);
-        if(_.isEmpty($damSize) || $damSize.length < 0){
+        if ($damSize === undefined || $damSize.length < 0) {
             return;
         }
 
-        if(_.isEmpty($formattedAssetSize) || $formattedAssetSize.length < 0){
+        if ($formattedAssetSize === undefined || $formattedAssetSize.length < 0) {
             return;
         }
         var sizeInBytes = $damSize[0].getAttribute("value");
-        $formattedAssetSize[0].value = (!sizeInBytes ?  "Unavailable" : formatBytes(parseInt(sizeInBytes), 2));
-        $formattedAssetSize[0].setAttribute("value", !sizeInBytes ?  "Unavailable" : formatBytes(parseInt(sizeInBytes), 2));
+        $formattedAssetSize[0].value = (!sizeInBytes ? "Unavailable" : formatBytes(parseInt(sizeInBytes), 2));
+        $formattedAssetSize[0].setAttribute("value", !sizeInBytes ? "Unavailable" : formatBytes(parseInt(sizeInBytes), 2));
     }
- 
+
     function formatBytes(bytes, decimals) {
-        if (bytes === 0){
+        if (bytes === 0) {
             return '0 Bytes';
         }
- 
+
         const k = 1024;
         const dm = decimals < 0 ? 0 : decimals;
         const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
         const i = Math.floor(Math.log(bytes) / Math.log(k));
- 
+
         return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
     }
 }(jQuery, jQuery(document)));
