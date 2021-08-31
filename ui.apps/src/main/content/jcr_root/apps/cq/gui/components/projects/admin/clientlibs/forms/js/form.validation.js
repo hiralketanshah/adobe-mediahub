70b2f9ff5b74c9e19fc9c308e7230a6688529229
abibrahi <abibrahi@adobe.com>
2021-08-31 17:34:10 +0530

    MED-137 changing the condition to top so that empty start date does not return string before validation

--- ui.apps/src/main/content/jcr_root/apps/cq/gui/components/projects/admin/clientlibs/forms/js/form.validation.js
+++ ui.apps/src/main/content/jcr_root/apps/cq/gui/components/projects/admin/clientlibs/forms/js/form.validation.js
@@ -88,6 +88,13 @@
                 return Granite.I18n.get("Due Date cannot be in the past");
             }
 
+            var plusone = new Date();
+            plusone.setFullYear(plusone.getFullYear() + 1);
+            if(setDateObj > plusone) {
+                errorDisableSubmit();
+                return Granite.I18n.get("Due Date cannot be more than 1 year from Current Date");
+            }
+
             var setStartDate = getDateFromCollection($('.project-startdate.coral-InputGroup'));
             if (setStartDate == null) {
                 // start date is not mandatory so return no error if it is null
@@ -98,13 +105,6 @@
             if (setDateObj < setStartDateObj) {
                 errorDisableSubmit();
                 return Granite.I18n.get("Due Date must be newer than Start Date");
-            }
-
-            var plusone = new Date();
-        	plusone.setFullYear(plusone.getFullYear() + 1);
-            if(setDateObj > plusone) {
-                errorDisableSubmit();
-                return Granite.I18n.get("Due Date cannot be more than 1 year from Current Date");
             }
 
             // wait for the ui to remove error icons before checking if there are any still displaying
