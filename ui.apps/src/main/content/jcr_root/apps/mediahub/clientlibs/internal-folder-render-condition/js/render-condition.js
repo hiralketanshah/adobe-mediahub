687b20016f65647ec5120ad505108d4b48e431cf
abibrahi <abibrahi@adobe.com>
2021-10-04 00:58:14 +0530

    MED-287 Changes to re oder private features

---
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/internal-folder-render-condition/js/render-condition.js
@@ -0,0 +1,46 @@
+(function ($, $document) {
+    $document.on("foundation-contentloaded", init);
+
+    function init() {
+       alert("init");
+       var privateCheckbox = $("#addprivaterestriction");
+
+        if(privateCheckbox) {
+            if(privateCheckbox.value == "true"){
+				document.getElementById("internalfolder").hidden = true;
+            	document.getElementById("internalusersection").hidden = true;
+            }
+
+            privateCheckbox.on('change', function handleChanged(event) {
+               if(event.currentTarget.checked){
+                document.getElementById("internalfolder").hidden = true;
+                document.getElementById("internalusersection").hidden = true;
+               } else {
+                document.getElementById("internalfolder").hidden = false;
+                document.getElementById("internalusersection").hidden = false;
+               }
+           });
+        } else {
+			document.getElementById("internalfolder").hidden = true;
+            document.getElementById("internalusersection").hidden = true;
+        }
+
+	  	var internalfolderCheckbox = $("#internalfolder");
+
+        if(internalfolderCheckbox) {
+            if(internalfolderCheckbox.value == "true"){
+            	document.getElementById("internalusersection").hidden = false;
+            }
+
+            internalfolderCheckbox.on('change', function handleChanged(event) {
+               if(event.currentTarget.checked){
+                document.getElementById("internalusersection").hidden = false;
+               } else {
+                document.getElementById("internalusersection").hidden = true;
+               }
+           });
+        }
+
+    }
+
+}(jQuery, jQuery(document)));
