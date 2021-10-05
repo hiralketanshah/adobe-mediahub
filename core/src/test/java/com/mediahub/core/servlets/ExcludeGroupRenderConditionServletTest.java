f0cbe56fe9130014831ad478df3fa5356430709c
Gregory Stievenard <gregory.stievenard@gmail.com>
2021-10-05 09:37:48 +0200

    Clean code

--- core/src/test/java/com/mediahub/core/servlets/ExcludeGroupRenderConditionServletTest.java
+++ core/src/test/java/com/mediahub/core/servlets/ExcludeGroupRenderConditionServletTest.java
@@ -1,38 +1,15 @@
 package com.mediahub.core.servlets;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.when;
-
 import com.adobe.granite.ui.components.Config;
 import com.adobe.granite.ui.components.rendercondition.RenderCondition;
 import com.adobe.granite.ui.components.rendercondition.SimpleRenderCondition;
 import com.mediahub.core.constants.BnpConstants;
-import com.mediahub.core.services.UserCreationService;
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import java.io.IOException;
-import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import javax.jcr.RepositoryException;
-import javax.jcr.UnsupportedRepositoryOperationException;
-import javax.jcr.Value;
-import javax.servlet.ServletException;
-import org.apache.jackrabbit.api.security.user.Authorizable;
 import org.apache.jackrabbit.api.security.user.Group;
 import org.apache.jackrabbit.api.security.user.User;
 import org.apache.jackrabbit.api.security.user.UserManager;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.SlingHttpServletResponse;
 import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -43,87 +20,89 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.osgi.service.component.annotations.Reference;
+
+import javax.jcr.RepositoryException;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(AemContextExtension.class)
 public class ExcludeGroupRenderConditionServletTest {
 
-  @InjectMocks
-  @Spy
-  private ExcludeGroupRenderConditionServlet fixture;
+    @InjectMocks
+    @Spy
+    private ExcludeGroupRenderConditionServlet fixture;
 
-  public AemContext context = new AemContext();
+    public AemContext context = new AemContext();
 
-  @Mock
-  User user;
+    @Mock
+    User user;
 
-  @Mock
-  SlingHttpServletRequest request;
+    @Mock
+    SlingHttpServletRequest request;
 
-  @Mock
-  ResourceResolver resourceResolver;
+    @Mock
+    ResourceResolver resourceResolver;
 
-  @Mock
-  UserCreationService userCreationService;
+    @Mock
+    ResourceResolverFactory resourceResolverFactory;
 
-  @Mock
-  ResourceResolverFactory resourceResolverFactory;
+    @Mock
+    Config config;
 
-  @Mock
-  Config config;
+    @Mock
+    UserManager userManager;
 
-  @Mock
-  UserManager userManager;
-
-  @Mock
-  Group group;
-
-  final Map<String, Object> authInfo = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE,
-      BnpConstants.WRITE_SERVICE);
-  MockSlingHttpServletResponse response;
-  Group userGroup;
-
-  @BeforeEach
-  public void setupMock() throws RepositoryException {
-    MockitoAnnotations.initMocks(this);
-    context.registerService(UserCreationService.class, userCreationService);
-    context.registerService(ResourceResolverFactory.class, resourceResolverFactory);
-    response = context.response();
-    final List<Group> groups = new ArrayList<>();
-    userGroup = mock(Group.class);
-    groups.add(userGroup);
-    when(request.getResourceResolver()).thenReturn(resourceResolver);
-    when(user.memberOf()).thenReturn(groups.iterator());
-  }
-
-  @Test
-  void doGet(AemContext context)
-      throws ServletException, IOException, RepositoryException, LoginException {
-    List<Group> groups = new ArrayList<>();
-    groups.add(group);
-
-    Iterator<Group> currentUserGroups = groups.iterator();
+    @Mock
+    Group group;
 
     final Map<String, Object> authInfo = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE,
-        BnpConstants.WRITE_SERVICE);
-    when(resourceResolverFactory.getServiceResourceResolver(authInfo)).thenReturn(resourceResolver);
-    when(request.getResourceResolver()).thenReturn(resourceResolver);
-    when(resourceResolver.getUserID()).thenReturn("admin");
-    when(resourceResolver.adaptTo(User.class)).thenReturn(user);
-    when(resourceResolver.adaptTo(UserManager.class)).thenReturn(userManager);
-    when(userManager.getAuthorizable("administrators")).thenReturn(group);
+            BnpConstants.WRITE_SERVICE);
+    MockSlingHttpServletResponse response;
+    Group userGroup;
 
-    when(user.memberOf()).thenReturn(currentUserGroups);
-    Resource resource = context.resourceResolver().getResource("/content/sample/en");
-    when(request.getResource()).thenReturn(resource);
-    SimpleRenderCondition show = new SimpleRenderCondition(true);
-    when(request.getAttribute(RenderCondition.class.getName())).thenReturn(show);
-    doReturn(new String[]{"administrators"}).when(fixture).getPropertiesFromConfig(request);
-    fixture.doGet(request, response);
-    assertEquals(show, request.getAttribute(RenderCondition.class.getName()));
-  }
+    @BeforeEach
+    public void setupMock() throws RepositoryException {
+        MockitoAnnotations.initMocks(this);
+        context.registerService(ResourceResolverFactory.class, resourceResolverFactory);
+        response = context.response();
+        final List<Group> groups = new ArrayList<>();
+        userGroup = mock(Group.class);
+        groups.add(userGroup);
+        when(request.getResourceResolver()).thenReturn(resourceResolver);
+        when(user.memberOf()).thenReturn(groups.iterator());
+    }
+
+    @Test
+    void doGet(AemContext context)
+            throws ServletException, IOException, RepositoryException, LoginException {
+        List<Group> groups = new ArrayList<>();
+        groups.add(group);
+
+        Iterator<Group> currentUserGroups = groups.iterator();
+
+        final Map<String, Object> authInfo = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE,
+                BnpConstants.WRITE_SERVICE);
+        when(resourceResolverFactory.getServiceResourceResolver(authInfo)).thenReturn(resourceResolver);
+        when(request.getResourceResolver()).thenReturn(resourceResolver);
+        when(resourceResolver.getUserID()).thenReturn("admin");
+        when(resourceResolver.adaptTo(User.class)).thenReturn(user);
+        when(resourceResolver.adaptTo(UserManager.class)).thenReturn(userManager);
+        when(userManager.getAuthorizable("administrators")).thenReturn(group);
+
+        when(user.memberOf()).thenReturn(currentUserGroups);
+        Resource resource = context.resourceResolver().getResource("/content/sample/en");
+        when(request.getResource()).thenReturn(resource);
+        SimpleRenderCondition show = new SimpleRenderCondition(true);
+        when(request.getAttribute(RenderCondition.class.getName())).thenReturn(show);
+        doReturn(new String[]{"administrators"}).when(fixture).getPropertiesFromConfig(request);
+        fixture.doGet(request, response);
+        assertEquals(show, request.getAttribute(RenderCondition.class.getName()));
+    }
 
 }
--- core/src/test/java/com/mediahub/core/servlets/TrackingRedirectServletTest.java
+++ core/src/test/java/com/mediahub/core/servlets/TrackingRedirectServletTest.java
@@ -1,84 +1,62 @@
 package com.mediahub.core.servlets;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-
 import com.day.cq.commons.jcr.JcrConstants;
-import com.mediahub.core.constants.BnpConstants;
-import com.mediahub.core.services.UserCreationService;
-
 import io.wcm.testing.mock.aem.junit5.AemContext;
 import io.wcm.testing.mock.aem.junit5.AemContextExtension;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import javax.jcr.RepositoryException;
-import javax.jcr.UnsupportedRepositoryOperationException;
-import javax.servlet.ServletException;
-
-import org.apache.jackrabbit.api.security.user.Group;
-import org.apache.jackrabbit.api.security.user.User;
 import org.apache.sling.api.SlingHttpServletRequest;
-import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.testing.mock.sling.servlet.MockSlingHttpServletResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(AemContextExtension.class)
 public class TrackingRedirectServletTest {
 
-	@InjectMocks
-	private TrackingRedirectServlet fixture;
+    @InjectMocks
+    private TrackingRedirectServlet fixture;
 
-	public AemContext context = new AemContext();
+    public AemContext context = new AemContext();
 
-	@Mock
-	SlingHttpServletRequest request;
+    @Mock
+    SlingHttpServletRequest request;
 
-	@Mock
-	ResourceResolver resourceResolver;
+    @Mock
+    ResourceResolver resourceResolver;
 
-	Resource res;
-	Resource resWithoutRedirect;
-	MockSlingHttpServletResponse response;
+    Resource res;
+    Resource resWithoutRedirect;
+    MockSlingHttpServletResponse response;
 
-	@BeforeEach
-	public void setupMock() {
-		MockitoAnnotations.initMocks(this);
-		res = context.create().resource("/content/test1", JcrConstants.JCR_PRIMARYTYPE, "dam:Asset", "redirectTarget",
-				"testTarget");
-		resWithoutRedirect = context.create().resource("/content/test2", JcrConstants.JCR_PRIMARYTYPE, "dam:Asset");
-		response = context.response();
-	}
+    @BeforeEach
+    public void setupMock() {
+        MockitoAnnotations.initMocks(this);
+        res = context.create().resource("/content/test1", JcrConstants.JCR_PRIMARYTYPE, "dam:Asset", "redirectTarget",
+                "testTarget");
+        resWithoutRedirect = context.create().resource("/content/test2", JcrConstants.JCR_PRIMARYTYPE, "dam:Asset");
+        response = context.response();
+    }
 
-	@Test
-	void doGetWithRedirect(AemContext context) throws IOException {
-		when(request.getResource()).thenReturn(res);
-		fixture.doGet(request, response);
-		assertEquals(302, response.getStatus());
-	}
+    @Test
+    void doGetWithRedirect(AemContext context) throws IOException {
+        when(request.getResource()).thenReturn(res);
+        fixture.doGet(request, response);
+        assertEquals(302, response.getStatus());
+    }
 
-	@Test
-	void doGetWithoutRedirect(AemContext context) throws IOException {
-		when(request.getResource()).thenReturn(resWithoutRedirect);
-		fixture.doGet(request, response);
-		assertEquals(200, response.getStatus());
-	}
+    @Test
+    void doGetWithoutRedirect(AemContext context) throws IOException {
+        when(request.getResource()).thenReturn(resWithoutRedirect);
+        fixture.doGet(request, response);
+        assertEquals(200, response.getStatus());
+    }
 }
