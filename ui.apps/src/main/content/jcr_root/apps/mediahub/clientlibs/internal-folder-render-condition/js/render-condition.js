304052f26fb1182f5bf7a923975e3d1c5dfb88c2
abibrahi <abibrahi@adobe.com>
2021-10-12 16:53:01 +0530

    Removing the listener for make private checkbox from MED-287 as the checkbox will not be there in /content/dam/projects

--- ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/internal-folder-render-condition/js/render-condition.js
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/internal-folder-render-condition/js/render-condition.js
@@ -2,53 +2,22 @@
     $document.on("foundation-contentloaded", init);
 
     function init() {
-       var privateCheckbox = $("#addprivaterestriction");
-       var internalfolderCheckbox = $("#internalfolder");
+      var internalfolderCheckbox = $("#internalfolder");
+      if(internalfolderCheckbox && document.getElementById("internaluserselection")) {
+          if($("#internalfolder")[0] && $("#internalfolder")[0].getAttribute("checked") === ''){
+              document.getElementById("internaluserselection").hidden = false;
+          } else {
+              document.getElementById("internaluserselection").hidden = true;
+          }
 
-        if(privateCheckbox) {
-
-            if($("#addprivaterestriction")[0] && $("#addprivaterestriction")[0].getAttribute("checked") === ''){
-                document.getElementById("internalfolder").hidden = true;
-                document.getElementById("internaluserselection").hidden = true;
-            } else {
-                document.getElementById("internalfolder").hidden = false;
-                document.getElementById("internaluserselection").hidden = false;
-            }
-
-            privateCheckbox.on('change', function handleChanged(event) {
-               if(event.currentTarget.checked){
-                document.getElementById("internalfolder").hidden = true;
-                document.getElementById("internaluserselection").hidden = true;
-               } else {
-                document.getElementById("internalfolder").hidden = false;
-                if(internalfolderCheckbox.checked){
-                  document.getElementById("internaluserselection").hidden = false;
-                }
-               }
-           });
-        } else {
-			      document.getElementById("internalfolder").hidden = true;
-            document.getElementById("internaluserselection").hidden = true;
-        }
-
-
-
-
-        if(internalfolderCheckbox) {
-            if($("#internalfolder")[0] && $("#internalfolder")[0].getAttribute("checked") === ''){
-                document.getElementById("internaluserselection").hidden = false;
-            } else {
-                document.getElementById("internaluserselection").hidden = true;
-            }
-
-            internalfolderCheckbox.on('change', function handleChanged(event) {
-               if(event.currentTarget.checked){
-                document.getElementById("internaluserselection").hidden = false;
-               } else {
-                document.getElementById("internaluserselection").hidden = true;
-               }
-           });
-        }
+          internalfolderCheckbox.on('change', function handleChanged(event) {
+             if(event.currentTarget.checked){
+              document.getElementById("internaluserselection").hidden = false;
+             } else {
+              document.getElementById("internaluserselection").hidden = true;
+             }
+         });
+      }
     }
 
 }(jQuery, jQuery(document)));
