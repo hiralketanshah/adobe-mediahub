a07ee05f89caaae1fa8608ff1de22105a83dfb34
abibrahi <abibrahi@adobe.com>
2022-01-04 18:07:33 +0530

    MED-473 Code review changes

--- core/src/main/java/com/mediahub/core/workflows/PublishScene7VideoAsset.java
+++ core/src/main/java/com/mediahub/core/workflows/PublishScene7VideoAsset.java
@@ -1,29 +1,16 @@
 package com.mediahub.core.workflows;
 
-import com.adobe.granite.workflow.WorkflowException;
 import com.adobe.granite.workflow.WorkflowSession;
 import com.adobe.granite.workflow.exec.WorkItem;
 import com.adobe.granite.workflow.exec.WorkflowProcess;
 import com.adobe.granite.workflow.metadata.MetaDataMap;
-import com.day.cq.commons.Externalizer;
-import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.dam.api.Asset;
 import com.day.cq.dam.commons.util.DamUtil;
-import com.day.cq.dam.scene7.api.S7Config;
-import com.day.cq.dam.scene7.api.Scene7Service;
-import com.day.cq.dam.scene7.api.constants.Scene7AssetType;
-import com.day.cq.dam.scene7.api.model.Scene7Asset;
-import com.day.cq.replication.Replicator;
 import com.mediahub.core.constants.BnpConstants;
-import com.mediahub.core.services.Scene7DeactivationService;
-import com.mediahub.core.utils.ReplicationUtils;
 import com.mediahub.core.utils.SlingJobUtils;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.sling.api.resource.ModifiableValueMap;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.event.jobs.JobManager;
@@ -35,7 +22,7 @@ import org.slf4j.LoggerFactory;
 /**
  * @author Abuthahir Ibrahim
  * <p>
- * Process step for Inter or External Activation of Asset
+ * Process step for External Activation of Video Asset
  */
 @Component(service = WorkflowProcess.class, immediate = true, property = {"process.label=MEDIAHUB : Publish Video Asset to Scene 7 if active"})
 public class PublishScene7VideoAsset implements WorkflowProcess {
@@ -48,9 +35,8 @@ public class PublishScene7VideoAsset implements WorkflowProcess {
     @Reference
     JobManager jobManager;
 
-
     @Override
-    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) throws WorkflowException {
+    public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap) {
         final Map<String, Object> authInfo = Collections.singletonMap(ResourceResolverFactory.SUBSERVICE, BnpConstants.WRITE_SERVICE);
         try (ResourceResolver resourceResolver = resolverFactory.getServiceResourceResolver(authInfo)) {
             String payloadPath = workItem.getWorkflowData().getPayload().toString();
@@ -62,7 +48,7 @@ public class PublishScene7VideoAsset implements WorkflowProcess {
                 }
             }
         } catch (Exception e) {
-            throw new WorkflowException("Error while activating asset in S7", e);
+            log.error("Error while Activating Video Asset {0}",e);
         }
 
     }
