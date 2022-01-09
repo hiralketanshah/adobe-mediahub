aa350f88e603864c89331ca978d050673f29f12b
abibrahi <abibrahi@adobe.com>
2022-01-10 05:20:47 +0530

    MED-484 Implementing not broadcast status

--- core/src/main/java/com/mediahub/core/workflows/ActivateWorkflowProcess.java
+++ core/src/main/java/com/mediahub/core/workflows/ActivateWorkflowProcess.java
@@ -10,6 +10,9 @@ import com.adobe.granite.workflow.model.WorkflowModel;
 import com.day.cq.commons.jcr.JcrConstants;
 import com.mediahub.core.constants.BnpConstants;
 import com.mediahub.core.utils.AssetUtils;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -17,12 +20,6 @@ import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ValueMap;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Map;
 
 /**
  * @author Abuthahir Ibrahim
@@ -32,7 +29,7 @@ import java.util.Map;
 @Component(service = WorkflowProcess.class, immediate = true, property = {"process.label=MEDIAHUB : Activate Asset"})
 public class ActivateWorkflowProcess implements WorkflowProcess {
 
-    private static final Logger log = LoggerFactory.getLogger(ActivateWorkflowProcess.class);
+    private static final String JCR_PATH = "JCR_PATH";
 
     @Reference
     ResourceResolverFactory resolverFactory;
@@ -40,7 +37,7 @@ public class ActivateWorkflowProcess implements WorkflowProcess {
     @Override
     public void execute(WorkItem workItem, WorkflowSession workflowSession, MetaDataMap metaDataMap)
             throws WorkflowException {
-        if (!workItem.getWorkflowData().getPayloadType().equals("JCR_PATH")) {
+        if (!workItem.getWorkflowData().getPayloadType().equals(JCR_PATH)) {
             throw new WorkflowException("Unable to get the payload");
         }
 
@@ -53,41 +50,11 @@ public class ActivateWorkflowProcess implements WorkflowProcess {
                 if (properties.containsKey(BnpConstants.BNPP_BROADCAST_STATUS)) {
                     String[] status = AssetUtils.getBroadcastStatus(properties, BnpConstants.BNPP_BROADCAST_STATUS);
                     if (Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_NOT_BROADCAST)) {
-                        // for future requirement
+                        // MED-484 Not Broadcast status
+                        deactivateAsset(workItem, workflowSession, properties, status);
                     } else {
-
-                        if (Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_EXTERNAL)) {
-                            String workflowName = "/var/workflow/models/mediahub/mediahub---external-publish";
-                            WorkflowModel wfModel = workflowSession.getModel(workflowName);
-                            WorkflowData wfData = workflowSession.newWorkflowData("JCR_PATH", workItem.getWorkflowData().getPayload().toString());
-                            workflowSession.startWorkflow(wfModel, wfData);
-                        }
-
-                        if (Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_INTERNAL)) {
-                            String workflowName = "/var/workflow/models/mediahub/mediahub---internal-publish";
-                            WorkflowModel wfModel = workflowSession.getModel(workflowName);
-                            WorkflowData wfData = workflowSession.newWorkflowData("JCR_PATH", workItem.getWorkflowData().getPayload().toString());
-                            workflowSession.startWorkflow(wfModel, wfData);
-                            if (payload.getParent() != null) {
-                                WorkflowData parentWorkflowData = workflowSession.newWorkflowData("JCR_PATH", payload.getParent().getPath());
-                                workflowSession.startWorkflow(wfModel, parentWorkflowData);
-                            }
-                        }
-
-                        if ((!Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_EXTERNAL)) && !StringUtils.isEmpty(properties.get(BnpConstants.BNPP_TRACKING_EXTERNAL_BROADCAST_URL, String.class))) {
-                            String workflowName = "/var/workflow/models/mediahub/mediahub---scene-7-deactivation";
-                            WorkflowModel wfModel = workflowSession.getModel(workflowName);
-                            WorkflowData wfData = workflowSession.newWorkflowData("JCR_PATH", workItem.getWorkflowData().getPayload().toString());
-                            workflowSession.startWorkflow(wfModel, wfData);
-                        }
-
-                        if ((!Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_INTERNAL)) && !StringUtils.isEmpty(properties.get(BnpConstants.BNPP_INTERNAL_BROADCAST_URL, String.class))) {
-                            String workflowName = "/var/workflow/models/mediahub/mediahub---internal-deactivation";
-                            WorkflowModel wfModel = workflowSession.getModel(workflowName);
-                            WorkflowData wfData = workflowSession.newWorkflowData("JCR_PATH", workItem.getWorkflowData().getPayload().toString());
-                            workflowSession.startWorkflow(wfModel, wfData);
-                        }
-
+                        activateAsset(workItem, workflowSession, payload, status);
+                        deactivateAsset(workItem, workflowSession, properties, status);
                     }
                 }
             }
@@ -95,5 +62,67 @@ public class ActivateWorkflowProcess implements WorkflowProcess {
             throw new WorkflowException("Error while publishing asset", e);
         }
 
+    }
+
+    /**
+     * Activate Asset or Media based on status
+     *
+     * @param workItem
+     * @param workflowSession
+     * @param payload
+     * @param status
+     * @throws WorkflowException
+     */
+    private void activateAsset(WorkItem workItem, WorkflowSession workflowSession, Resource payload,
+        String[] status) throws WorkflowException {
+        if (Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_EXTERNAL)) {
+            String workflowName = "/var/workflow/models/mediahub/mediahub---external-publish";
+            WorkflowModel wfModel = workflowSession.getModel(workflowName);
+            WorkflowData wfData = workflowSession.newWorkflowData(
+                JCR_PATH, workItem.getWorkflowData().getPayload().toString());
+            workflowSession.startWorkflow(wfModel, wfData);
+        }
+
+        if (Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_INTERNAL)) {
+            String workflowName = "/var/workflow/models/mediahub/mediahub---internal-publish";
+            WorkflowModel wfModel = workflowSession.getModel(workflowName);
+            WorkflowData wfData = workflowSession.newWorkflowData(
+                JCR_PATH, workItem.getWorkflowData().getPayload().toString());
+            workflowSession.startWorkflow(wfModel, wfData);
+            if (payload.getParent() != null) {
+                WorkflowData parentWorkflowData = workflowSession.newWorkflowData(
+                    JCR_PATH, payload.getParent().getPath());
+                workflowSession.startWorkflow(wfModel, parentWorkflowData);
+            }
+        }
+    }
+
+    /**
+     * Deactivate asset or media based on status. It is also used to toggle between External and Internal publish
+     *
+     * @param workItem
+     * @param workflowSession
+     * @param properties
+     * @param status
+     * @throws WorkflowException
+     */
+    private void deactivateAsset(WorkItem workItem, WorkflowSession workflowSession,
+        ValueMap properties, String[] status) throws WorkflowException {
+        if ((!Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_EXTERNAL)) && !StringUtils
+            .isEmpty(properties.get(BnpConstants.BNPP_TRACKING_EXTERNAL_BROADCAST_URL, String.class))) {
+            String workflowName = "/var/workflow/models/mediahub/mediahub---scene-7-deactivation";
+            WorkflowModel wfModel = workflowSession.getModel(workflowName);
+            WorkflowData wfData = workflowSession.newWorkflowData(
+                JCR_PATH, workItem.getWorkflowData().getPayload().toString());
+            workflowSession.startWorkflow(wfModel, wfData);
+        }
+
+        if ((!Arrays.asList(status).contains(BnpConstants.BROADCAST_VALUE_INTERNAL)) && !StringUtils.isEmpty(properties.get(BnpConstants.BNPP_INTERNAL_BROADCAST_URL, String.class))) {
+            String workflowName = "/var/workflow/models/mediahub/mediahub---internal-deactivation";
+            WorkflowModel wfModel = workflowSession.getModel(workflowName);
+            WorkflowData wfData = workflowSession.newWorkflowData(
+                JCR_PATH, workItem.getWorkflowData().getPayload().toString());
+            workflowSession.startWorkflow(wfModel, wfData);
+        }
     }
 }
