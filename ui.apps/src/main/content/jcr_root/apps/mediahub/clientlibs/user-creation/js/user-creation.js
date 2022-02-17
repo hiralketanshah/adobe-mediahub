324f27e186920c53e037d31b34501771d2917e0a
abibrahi <abibrahi@adobe.com>
2022-02-17 20:29:00 +0530

    MED-542 user profile additional changes

--- ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/user-creation/js/user-creation.js
+++ ui.apps/src/main/content/jcr_root/apps/mediahub/clientlibs/user-creation/js/user-creation.js
@@ -5,9 +5,11 @@
       var millisecondsToWait = 700;
 
       setTimeout(function() {
+        var isTechnicalAdmin = document.getElementById("user.technical.admin");
+        var isManager = document.getElementById("user.entity.project.manager");
+        var userType = document.getElementById("user.type");
         // Changes as per MED-224
         if($("coral-select[name='./profile/type']") && $("coral-select[name='./profile/type']").length > 0){
-
           var activities = $("coral-select[name='./profile/type']")[0];
           activities.addEventListener("change", function() {
               if(activities.value == "internal"){
@@ -16,85 +18,69 @@
                 $("coral-datepicker[name='./profile/expiry']")[0].disabled = false;
               }
           });
-
-          if(!isAdmin() && $("coral-select[name='./profile/type']")[0].value === "internal" && $("input[name='./profile/email']")[0].value !== ""){
-
-            $("coral-datepicker[name='./profile/expiry']")[0].disabled = true;
-			$("input[name='./profile/email']")[0].disabled = true;
-			$("input[name='./profile/givenName']")[0].disabled = true;
-			$("input[name='./profile/familyName']")[0].disabled = true;
-			$("input[name='./profile/company']")[0].disabled = true;
-			$("input[name='./profile/status']")[0].disabled = true;
-			$("coral-select[name='./profile/type']")[0].disabled = true;
-			$("input[name='./profile/pole']")[0].disabled = true;
-			$("input[name='./profile/uo']")[0].disabled = true;
-			$("input[name='./profile/jobTitle']")[0].disabled = true;
-			$("input[name='./profile/city']")[0].disabled = true;
-			$("coral-select[name='./profile/country']")[0].disabled = true;
-          } else {
-            $("coral-datepicker[name='./profile/expiry']")[0].disabled = false;
+          // changes as per MED-542
+          hideProfileElements(true);
+          $("coral-select[name='./profile/type']")[0].disabled = true;
+          if( (isTechnicalAdmin.value === "true" || isManager.value === "true") && userType.value === "external"){
+              showProfileElements();
+          } else if(isTechnicalAdmin.value === "true" && userType.value === "internal"){
+              showProfileElements();
+              $("coral-select[name='./profile/type']")[0].removeAttribute("disabled");
           }
         }
-		if(isCurrentExternalUser()){
-			$("coral-datepicker[name='./profile/expiry']")[0].disabled = true;
-            $("coral-select[name='./profile/type']")[0].disabled = true;
-        }
-
 
         if($("coral-datepicker[name='./profile/expiry']") && $("coral-datepicker[name='./profile/expiry']").length > 0){
-            var interimDate = moment(new Date());
-          	interimDate.add(365, "days");  //display 365 days prior to today
-			      $("coral-datepicker[name='./profile/expiry']")[0].max = interimDate.format("YYYY-MM-DD");
+          var interimDate = moment(new Date());
+          interimDate.add(365, "days");  //display 365 days prior to today
+          $("coral-datepicker[name='./profile/expiry']")[0].max = interimDate.format("YYYY-MM-DD");
         }
-		
-		
 
       }, millisecondsToWait);
-	  
-	  function isCurrentExternalUser() {
-        var currentUser = getCurrentUser();
-        var type;
-		$.ajax( {
-            url: "/apps/mediahub/projects/teammembers/datasource.userinfo.json?userid="+currentUser,
-            async: false
-        } ).done(handler);
-        function handler(data){
-            type = data.type;
-        }
-        if(type === "external"){
-			return true;
-        }
-        return false;
-	  }	
-	  
-	  function isAdmin() {
-        var currentUser = getCurrentUser();
-        var isAdmin;
-		$.ajax( {
-            url: "/apps/mediahub/projects/teammembers/datasource.userinfo.json?userid="+currentUser,
-            async: false
-        } ).done(handler);
-        function handler(data){
-            isAdmin = data.isAdmin;
-        }
+    }
 
-        if(isAdmin === true){
-			return true;
-        }
-        return false;
-	  }	
+    function hideProfileElements(show){
+      disableFields($("coral-datepicker[name='./profile/expiry']"));
+      disableFields($("input[name='./profile/email']"));
+      disableFields($("input[name='./profile/givenName']"));
+      disableFields($("input[name='./profile/familyName']"));
+      disableFields($("input[name='./profile/company']"));
+      disableFields($("input[name='./profile/status']"));
 
-      function getCurrentUser() {
-		var currentUserId;
-        var result = Granite.$.ajax({
-			type: "GET",
-            async: false,
-            url: Granite.HTTP.externalize("/libs/granite/security/currentuser.json")
-        });
-        if (result.status === 200) {
-            currentUserId = JSON.parse(result.responseText).authorizableId;
-        }
-        return currentUserId;
+      disableFields($("input[name='./profile/pole']"));
+      disableFields($("input[name='./profile/uo']"));
+      disableFields($("input[name='./profile/jobTitle']"));
+      disableFields($("input[name='./profile/city']"));
+      disableFields($("coral-select[name='./profile/country']"));
+      disableFields($("coral-datepicker[name='./profile/expiry']"));
+    }
+
+    function showProfileElements(){
+      enableFields($("coral-datepicker[name='./profile/expiry']"));
+      enableFields($("input[name='./profile/email']"));
+      enableFields($("input[name='./profile/givenName']"));
+      enableFields($("input[name='./profile/familyName']"));
+      enableFields($("input[name='./profile/company']"));
+      enableFields($("input[name='./profile/status']"));
+
+      enableFields($("input[name='./profile/pole']"));
+      enableFields($("input[name='./profile/uo']"));
+      enableFields($("input[name='./profile/jobTitle']"));
+      enableFields($("input[name='./profile/city']"));
+      enableFields($("coral-select[name='./profile/country']"));
+      enableFields($("coral-datepicker[name='./profile/expiry']"));
+    }
+
+    function disableFields(element){
+      if(element && element.length > 0){
+        element[0].disabled = true;
       }
     }
+
+    function enableFields(element){
+      if(element && element.length > 0){
+        element[0].removeAttribute("disabled");
+      }
+    }
+
+
 }(jQuery, jQuery(document)));
